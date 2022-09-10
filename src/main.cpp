2bd056002784e22f46bc2f79140d36eb43fa40ad
ssaattww <taiga202500@gmail.com>
2022-09-10 15:20:33 +0900

    succesed

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,8 @@
 #include <Arduino.h>
 #include <M5Core2.h>
 
+#include <Encoding/COBS.h>
+
 typedef union{
   uint8_t bytes[sizeof(char)*6];
   struct 
@@ -15,47 +17,75 @@ typedef union{
     signed char hWheel;// yoko
   }B;
 }MousePos;
-void onPacketReceived(const void* sender, const uint8_t* buffer, size_t size);
 
+// task
+void displayTask(void* args);
+
+// mouse
+void moveMouse();
 BleMouse bleMouse;
-PacketSerial myPacketSerial;
 MousePos pos;
-bool handled = false;
+int count = 0;        // moved count
+
+// serial
+void serialArrivedEvent();
+bool arrived = false;
+uint8_t buffer[64];
+size_t bufferSize;
+
 void setup() {
   M5.begin();
-  myPacketSerial.setPacketHandler(&onPacketReceived);
+  xTaskCreatePinnedToCore(displayTask, "displayTask", 4096, NULL, 2, NULL, 0);
   Serial.begin(115200);
-  myPacketSerial.setStream(&Serial);
   M5.Lcd.fillScreen(WHITE);
   M5.Lcd.setTextColor(BLACK, WHITE);
   bleMouse.begin();
 }
 
 void loop() {
-  int count = 0;
-  myPacketSerial.update();
-  if(bleMouse.isConnected() && handled == false){
+  if(arrived){
+    serialArrivedEvent();
+    moveMouse();
+  }
+}
+
+void serialEvent(){
+  arrived = true;
+  bufferSize = Serial.readBytes(buffer,64);
+}
+
+void moveMouse(){
+  if(bleMouse.isConnected()){
       if(pos.B.pressing == 1){
         bleMouse.press(pos.B.buttons);
-      }
-      else if(pos.B.pressing == 0){
+      }else if(pos.B.pressing == 0){
         bleMouse.release(pos.B.buttons);
       }
+
       bleMouse.move(pos.B.x, pos.B.y, pos.B.wheel, pos.B.hWheel);
-      handled = true;
-      M5.Lcd.setCursor(0, 0);
-      M5.Lcd.printf("%d:%d,%d,%d,%d        ",count, pos.B.x, pos.B.y, pos.B.hWheel, pos.B.hWheel);
       count++;
   }
 }
 
-void onPacketReceived(const void* sender, const uint8_t* buffer, size_t size){
-  if(sender == &myPacketSerial && size == 6){
+void serialArrivedEvent(){
+  uint8_t decoded[64];
+  uint8_t encoded[64];
+  size_t decodedSize = COBS::decode(buffer, bufferSize, decoded);
+  size_t encodedSize = COBS::encode(decoded, decodedSize, encoded);
+  if(decodedSize == 6){
     for(int i=0;i<6;i++){
-        pos.bytes[i] = buffer[i];
+      pos.bytes[i] = decoded[i];
     }
-    handled = false;
-    M5.Lcd.setCursor(20, 0);
-    M5.Lcd.printf("recieved");
+  }
+  Serial.write(encoded,encodedSize);
+  arrived = false;
+}
+
+void displayTask(void* args){
+  while (true)
+  {
+    M5.Lcd.setCursor(0, 0);
+    M5.Lcd.printf("%d:%d,%d,%d,%d        ",count, pos.B.x, pos.B.y, pos.B.hWheel, pos.B.hWheel);
+    delay(100);
   }
 }
